e17c96eb6d8b4b691449a9c02016dde02f0431af
Funnyhust <hustvanduong@gmail.com>
2025-12-04 00:28:07 +0700

    add i2c mềm

--- src/services/software_i2c/software_i2c.c
+++ src/services/software_i2c/software_i2c.c
@@ -1,6 +1,5 @@
 // Note các chân sử dụng cho phần mềm I2C
 /*
-
 PE13 TIM1_CH3
 PE14 TIM1_CH4          
 PE15 TIM1_BKIN       
@@ -12,3 +11,134 @@ PD9 FSMC_D14
 PD10 FSMC_D15
 PD11 FSMC_A16
 */
+
+#include "software_i2c.h"
+
+// Biến global để lưu chân hiện tại đang sử dụng
+uint8_t SCL_PIN = SCL_PIN_1;
+uint8_t SDA_PIN = SDA_PIN_1;
+
+// Hàm delay cho I2C (tần số ~50 kHz với delay 10us)
+void i2c_delay() {
+    delayMicroseconds(10);
+}
+
+// Chọn bus I2C (1-5)
+void i2c_select_bus(uint8_t bus_num) {
+    switch(bus_num) {
+        case 1:
+            SCL_PIN = SCL_PIN_1;
+            SDA_PIN = SDA_PIN_1;
+            break;
+        case 2:
+            SCL_PIN = SCL_PIN_2;
+            SDA_PIN = SDA_PIN_2;
+            break;
+        case 3:
+            SCL_PIN = SCL_PIN_3;
+            SDA_PIN = SDA_PIN_3;
+            break;
+        case 4:
+            SCL_PIN = SCL_PIN_4;
+            SDA_PIN = SDA_PIN_4;
+            break;
+        case 5:
+            SCL_PIN = SCL_PIN_5;
+            SDA_PIN = SDA_PIN_5;
+            break;
+        default:
+            // Mặc định bus 1 nếu bus_num không hợp lệ
+            SCL_PIN = SCL_PIN_1;
+            SDA_PIN = SDA_PIN_1;
+            break;
+    }
+}
+
+// Khởi tạo I2C
+void i2c_init() {
+    pinMode(SCL_PIN, OUTPUT);
+    pinMode(SDA_PIN, OUTPUT);
+    SCL_HIGH();
+    SDA_HIGH();
+    i2c_delay();
+}
+
+// Tạo điều kiện START
+void i2c_start() {
+    SDA_HIGH();
+    i2c_delay();
+    SCL_HIGH();
+    i2c_delay();
+    SDA_LOW();
+    i2c_delay();
+    SCL_LOW();
+    i2c_delay();
+}
+
+// Tạo điều kiện STOP
+void i2c_stop() {
+    SDA_LOW();
+    i2c_delay();
+    SCL_HIGH();
+    i2c_delay();
+    SDA_HIGH();
+    i2c_delay();
+}
+
+// Ghi 1 byte và đọc ACK
+bool i2c_write_byte(uint8_t data) {
+    for (uint8_t i = 0; i < 8; i++) {
+        if (data & 0x80) {
+            SDA_HIGH();
+        } else {
+            SDA_LOW();
+        }
+        i2c_delay();
+        SCL_HIGH();
+        i2c_delay();
+        SCL_LOW();
+        i2c_delay();
+        data <<= 1;
+    }
+    
+    // Release SDA để đọc ACK
+    pinMode(SDA_PIN, INPUT_PULLUP);
+    SDA_HIGH();
+    i2c_delay();
+    SCL_HIGH();
+    i2c_delay();
+    bool ack = (digitalRead(SDA_PIN) == LOW); // ACK = LOW
+    SCL_LOW();
+    i2c_delay();
+    pinMode(SDA_PIN, OUTPUT);
+    return ack;
+}
+
+// Đọc 1 byte và gửi ACK/NACK
+uint8_t i2c_read_byte(bool ack) {
+    uint8_t data = 0;
+    pinMode(SDA_PIN, INPUT_PULLUP);
+    
+    for (uint8_t i = 0; i < 8; i++) {
+        SCL_HIGH();
+        i2c_delay();
+        data = (data << 1) | digitalRead(SDA_PIN);
+        SCL_LOW();
+        i2c_delay();
+    }
+    
+    // Gửi ACK/NACK
+    pinMode(SDA_PIN, OUTPUT);
+    if (ack) {
+        SDA_LOW();  // ACK
+    } else {
+        SDA_HIGH(); // NACK
+    }
+    i2c_delay();
+    SCL_HIGH();
+    i2c_delay();
+    SCL_LOW();
+    i2c_delay();
+    
+    return data;
+}
---
+++ src/services/software_i2c/software_i2c.h
@@ -0,0 +1,40 @@
+#ifndef SOFTWARE_I2C_H
+#define SOFTWARE_I2C_H
+
+#include <Arduino.h>
+
+// Định nghĩa các cặp chân I2C
+#define SCL_PIN_1 PE13
+#define SDA_PIN_1 PE14
+#define SCL_PIN_2 PE15
+#define SDA_PIN_2 PB13
+#define SCL_PIN_3 PB14
+#define SDA_PIN_3 PB15
+#define SCL_PIN_4 PD8
+#define SDA_PIN_4 PD9
+#define SCL_PIN_5 PD10
+#define SDA_PIN_5 PD11
+
+// Biến global để lưu chân hiện tại đang sử dụng
+extern uint8_t SCL_PIN;
+extern uint8_t SDA_PIN;
+
+// Thao tác chân cơ bản
+#define SDA_HIGH() digitalWrite(SDA_PIN, HIGH)
+#define SDA_LOW()  digitalWrite(SDA_PIN, LOW)
+#define SCL_HIGH() digitalWrite(SCL_PIN, HIGH)
+#define SCL_LOW()  digitalWrite(SCL_PIN, LOW)
+
+// Hàm chọn bus I2C (1-5)
+void i2c_select_bus(uint8_t bus_num);
+
+// Hàm khởi tạo I2C
+void i2c_init();
+
+// Các hàm giao tiếp I2C
+void i2c_start();
+void i2c_stop();
+bool i2c_write_byte(uint8_t data);
+uint8_t i2c_read_byte(bool ack);
+
+#endif // SOFTWARE_I2C_H
